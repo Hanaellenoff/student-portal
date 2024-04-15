2fc3ac7cd7d7a91acde2ae115a255072ba3cf451
Hanaellenoff <hanaellenoff@gmail.com>
2024-04-15 16:28:22 -0400

    messup

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,27 +1,7 @@
-import { ResumeModal } from "./ResumeModal";
-
-import { ResumeShow } from "./ResumeShow";
-// import { Routes, Route } from "react-router-dom";
-
-import { Login } from "./Login";
-import { LogoutLink } from "./LogoutLink";
-
-
 export function Content() {
-  const resume = [
-    {
-      id: 1,
-      link: "google.com",
-    },
-    {
-      id: 2,
-      link: "amazon.com",
-    },
-  ];
   return (
     <div>
-      <ResumeShow resume={resume} />
-      <ResumeModal></ResumeModal>
+      <h2>Test</h2>
     </div>
   );
 }
