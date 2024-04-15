767ee2d6a1ccc8705d228e9857264d744b2b1eb9
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-04-15 16:17:46 -0400

    added login input to home page

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,26 +1,13 @@
 import { ResumeModal } from "./ResumeModal";
-import { LoginModal } from "./LoginModal";
 import { useState } from "react";
-import { Login } from "./Login";
+import { Routes, Route } from "react-router-dom";
 
 import { ResumeShow } from "./ResumeShow";
-// import { Routes, Route } from "react-router-dom";
 
 import { Login } from "./Login";
 import { LogoutLink } from "./LogoutLink";
 
-
 export function Content() {
-  const [isLoginShowVisible, setIsLoginShowVisible] = useState(false);
-
-  const handleShowLogin = () => {
-    setIsLoginShowVisible(true);
-  };
-
-  const handleClose = () => {
-    setIsLoginShowVisible(false);
-  };
-
   const resume = [
     {
       id: 1,
@@ -33,17 +20,8 @@ export function Content() {
   ];
   return (
     <div>
-<<<<<<< HEAD
-      <ResumeModal show={true}>
-        <h1>test</h1>
-      </ResumeModal>
-      <LoginModal show={isLoginShowVisible}>
-        <Login />
-      </LoginModal>
-=======
       <ResumeShow resume={resume} />
       <ResumeModal></ResumeModal>
->>>>>>> 7bf38ed791c5516c8e600f2855e87b0bb2ccc788
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { Login } from "./Login";
 import { useState } from "react";
-import { LoginModal } from "./LoginModal";
+import { Login } from "./Login";
 
 export function Header() {
-  const [isLoginVisible, setIsLoginVisible] = useState(false);
-
-  const handleShowLogin = () => {
-    setIsLoginVisible(true);
-  };
-
   return (
     <header>
       <div className="navbar">
-        <h1>Student Portal</h1>
+        <h2>Student Portal</h2>
         <nav>
           <a href="#">
             <button>Resume</button>
@@ -22,11 +15,11 @@ export function Header() {
             <button>Capstone</button>
           </a>
           <a href="#">
-            <button onClick={handleShowLogin}>Login</button>
-          </a>
-          <a href="#">
             <button>Log Out</button>
           </a>
+          <div className="loginlink">
+            <Login />
+          </div>
         </nav>
       </div>
     </header>
