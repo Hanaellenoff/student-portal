8f510bc3768641efeeaef1934ae1f8e7968cfb3e
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-04-16 13:58:56 -0400

    deleted broken code

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,5 @@
 export function Home() {
   return (
-    <div className="home">
-      <h1>Welcome to the Student Portal!</h1>
-      <p>Log in with you school email and password, then view and update your current resume and capstone projects.</p>
-    </div>
+
   );
 }
