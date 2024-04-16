c759de9db96c46d3fde2f07b5607f38dfb7337c2
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-04-16 14:13:09 -0400

    added capstone and resume files

--- src/Capstone.jsx
+++ src/Capstone.jsx
@@ -1,3 +1,16 @@
 export default function Capstone() {
-  return <h1>Test</h1>;
+  const capstone = {
+    linkedin_url: "linkedin.url",
+    twitter_handle: "twitter.url",
+    github_url: "github.url",
+  };
+
+  return (
+    <div>
+      <h1>Capstone</h1>
+      <p>{capstone.linkedin_url}</p>
+      <p>{capstone.twitter_handle}</p>
+      <p>{capstone.github_url}</p>
+    </div>
+  );
 }
