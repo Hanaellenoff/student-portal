ff9dd78e5b64d2a05b6231cf1af2cd4fffeb04e5
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-04-16 14:52:03 -0400

    added user file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,33 @@ import "./App.css";
 import Capstone from "./Capstone";
 import Resume from "./Resume";
 import { Login } from "./Login";
+import User from "./User";
 
 function App() {
+  const capstone = {
+    name: "Cool Project",
+    description: "This is how the project works and how I did it.",
+    url: "capstone.url",
+    screenshot: "screenshot image",
+  };
+
+  const user = {
+    first_name: "Zach",
+    last_name: "Engel",
+    phone_number: "123-456-7890",
+    email: "test@email.com",
+    short_bio: "Example bio",
+    linkedin_url: "linkedin.com",
+    twitter_handle: "twitter.com",
+    personal_url: "personal.com",
+    github_url: "github.com",
+    photo: "photo",
+  };
   return (
     <div>
       <Login />
-      <Capstone />
+      <User user={user} />
+      <Capstone capstone={capstone} />
       <Resume />
     </div>
   );
